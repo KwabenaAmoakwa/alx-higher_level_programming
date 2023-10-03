4502e5718c96ea37770dbc43bfd11654179def32
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-03 12:38:40 -0700

    12-fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    temp = ""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 != 0:
+            temp += "Fizz "
+        elif i % 3 != 0 and i % 5 == 0:
+            temp += "Buzz "
+        elif i % 3 == 0 and i % 5 == 0:
+            temp += "FizzBuzz "
+        else:
+            temp += str(i) + " "
+    print(temp, end=" ")
