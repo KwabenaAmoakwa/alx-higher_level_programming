109519d9653c8e2685c7f2066057f624811c897e
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-03 09:37:16 -0700

    8-upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    temp = ""
+    for i in str:
+        if 97 <= ord(i) <= 122 and i != " " and '0':
+            temp += chr((ord(i)-97)+65)
+        else:
+            temp += i
+    print("{}".format(temp))
