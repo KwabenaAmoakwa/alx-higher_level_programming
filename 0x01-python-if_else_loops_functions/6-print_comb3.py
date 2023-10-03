9050f87b28f5b8c75c1b4f03dc4e1110b02150ba
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-03 05:43:59 -0700

    6-print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0,10):
-    for x in range(0,10):
+for i in range(0, 10):
+    for x in range(0, 10):
         if i < x and i != x and i < 8:
-            print(f"{i}{x}, ",end = "")
+            print("{}{}, ".format(i, x), end="")
 print(89)
-            
