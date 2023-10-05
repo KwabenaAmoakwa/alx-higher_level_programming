056594dcec1a1af4255ca65081f7f9ce7546dc7b
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-05 09:34:44 -0700

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ =="__main__":
+    if len(argv) == 1:
+        print("{} argument{}".format(len(argv) - 1,"s."))
+    elif len(argv) == 2:
+        print("{} argument{}".format(len(argv) - 1,"."))
+        print("{}: {}".format(len(argv) - 1, argv[1]))
+    else:
+        print("{} argument{}".format(len(argv) - 1,"s:"))
+        c = 0
+        for i in argv:
+            if i != argv[0]:
+                c += 1
+                print("{}: {}".format(c, i))
