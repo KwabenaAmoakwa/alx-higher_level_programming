fa0f49c3a86a80e80fd4b64b482787e1bd06db37
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-07 11:38:47 -0700

    7-add_tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    [tuple_a1, tuple_b1] = [[], []]
+
+    if len(tuple_a) >= 2 and len(tuple_b) == 2:
+        [tuple_a1, tuple_b1] = [list(tuple_a), list(tuple_b)]
+    elif len(tuple_a) >= 2 and len(tuple_b) != 2:
+        tuple_a1 = list(tuple_a)
+        tuple_b1 = [tuple_b[0], 0] if len(tuple_b) > 0 else [0, 0]
+    elif len(tuple_b) >= 2 and len(tuple_a) != 2:
+        tuple_b1 = list(tuple_b)
+        tuple_a1 = [tuple_a[0], 0] if len(tuple_a) > 0 else [0, 0]
+    else:
+        tuple_a1 = [tuple_a[0], 0] if len(tuple_a) > 0 else [0, 0]
+        tuple_b1 = [tuple_b[0], 0] if len(tuple_b) > 0 else [0, 0]
+    return (tuple_a1[0] + tuple_b1[0], tuple_a1[1] + tuple_b1[1])
