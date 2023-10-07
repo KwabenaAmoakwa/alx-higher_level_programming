87bda83a48f51ef43d0449b43d01be270e718798
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-07 05:31:00 -0700

    6-print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) > 0:
         for i in matrix:
             if len(i) > 0:
-                print("{}".format(i[0]), end="")
+                print("{:d}".format(i[0]), end="")
                 for x in range(1, len(i)):
-                    print(" {}".format(i[x]), end="")
+                    print(" {:d}".format(i[x]), end="")
             print("{}".format(''))
     else: print("{}".format(''))
