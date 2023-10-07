ae45fb3251ef2c770718d987d9cd2b6eba59da93
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-07 12:13:11 -0700

    9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+    if len(my_list) > 0:
+        maxy = my_list[0]
+        for i in my_list:
+            if i > maxy:
+                maxy = i
+        return maxy
+    return None
 
