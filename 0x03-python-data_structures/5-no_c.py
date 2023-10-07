606804759fa792dda47dc35e54ba7624205fd92d
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-07 04:28:11 -0700

    5-n0_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,10 @@
 
 def no_c(my_string):
     nl = list(my_string)
-    for i in nl:
-        if i == "c" or i == "C":
-            nl.remove(i)
+    if 'c' in nl:
+        for i in nl:
+            if i == "c" or i == "C":
+                nl.remove(i)
+    else:
+        return
     return ('').join(nl)
