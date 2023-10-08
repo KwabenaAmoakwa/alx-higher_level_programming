4f9e9b3161b48ea6a2ba908fb3b4e8182283a879
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-08 06:23:32 -0700

    8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,6 @@
 
 def multiple_returns(sentence):
     if len(sentence) > 0:
-        return(len(sentence), sentence[0])
-    return(len(sentence), None)
+        return len(sentence), sentence[0]
+    else:
+        return len(sentence), None
