30437cad2c0de8e1eb6fde06871134d0dcf0979a
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-11 06:42:03 -0700

    6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    li = list(a_dictionary.keys())
+    li.sort()
+    for  i in range(len(li)):
+        print("{}: {}".format(li[i], a_dictionary[li[i]]))
