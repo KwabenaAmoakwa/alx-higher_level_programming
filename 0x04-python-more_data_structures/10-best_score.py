878bae9679651ab38cb0e9c1a48a65468ede540f
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-11 09:53:03 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    hs,hk = 0,''
+    hs,hk = 0, ''
     if a_dictionary != None:
         for i in a_dictionary.keys():
             if a_dictionary[i] > hs:
