9dd7babc7d943c07791314704d88b5b45cc30b9c
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-11 12:15:01 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     hs, hk = 0, ''
-    if not a_dictionary None:
+    if a_dictionary is not None:
         for i in a_dictionary.keys():
             if a_dictionary[i] > hs:
                 hs = a_dictionary[i]
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    if not a_dictionary None:
+    if a_dictionary is not None:
         li = list(a_dictionary.keys())
         li.sort()
         for i in range(len(li)):
