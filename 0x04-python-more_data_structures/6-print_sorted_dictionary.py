60c187b630efb76a19808545451b385a2b00a52c
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-11 08:36:01 -0700

    7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    li = list(a_dictionary.keys())
-    li.sort()
-    for i in range(len(li)):
-        print("{}: {}".format(li[i], a_dictionary[li[i]]))
+    if a_dictionary != None:
+        li = list(a_dictionary.keys())
+        li.sort()
+        for i in range(len(li)):
+            print("{}: {}".format(li[i], a_dictionary[li[i]]))
