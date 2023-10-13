e7d0f80f8ab969bbed69c49d3d56958a7479a350
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-13 02:56:36 -0700

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if type(roman_string) != 'string':
+        return 0
     ones = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5, 'VI': 6, 'VII': 7,
             'VIII': 8,
             'IX': 9,
             }
-    tens = {'X': 10,
-            'XX': 20,
-            'XXX': 30,
-            'XL': 40,
-            'L': 50,
-            'LX': 60,
+    tens = {'X': 10, 'XX': 20, 'XXX': 30, 'XL': 40, 'L': 50, 'LX': 60,
             'LXX': 70,
             'LXXX': 80,
             'XC': 90,
             }
-    hundreds = {'C': 100,
-                'CC': 200,
-                'CCC': 300,
-                'CD': 400,
-                'D': 500,
+    hundreds = {'C': 100, 'CC': 200, 'CCC': 300, 'CD': 400, 'D': 500,
                 'DC': 600,
                 'DCC': 700,
                 'DCCC': 800,
