cdddc568257f2ec66adc33195986d3729062f9a2
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-13 12:44:21 -0700

    13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,6 @@
 def weight_average(my_list=[]):
     if my_list == []:
         return 0
-    det = sum([ i[1] for i in my_list])
+    det = sum([i[1] for i in my_list])
     av = sum([i[0] * i[1] for i in my_list]) / det
-    return av   
+    return av
