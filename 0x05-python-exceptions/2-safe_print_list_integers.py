5f006d0c238ef46d3eb46cf80563ec8f6eec97f9
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-23 11:48:47 -0700

    2-main

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 n += 1
         print("")
         return n
-    except IndexError:
-        print("")
-        return n
+    except TypeError:
+        print("", end="")
+
