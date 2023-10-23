63144a0bf506a175bf21f43ddc34666a4fa5c75b
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-23 05:57:02 -0700

    0-main

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    n = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end = "")
+            print("{}".format(my_list[i]), end="")
+            n+=1
         print("")
-        return my_list[x-1]
+        return n
     except IndexError:
         print("")
-        return my_list[-1]
+        return n
