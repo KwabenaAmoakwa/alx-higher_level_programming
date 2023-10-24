0fec9a26fd74f13f1c5d4506dae59a9bf65b1fd6
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-24 09:18:39 -0700

    1-main

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
 class Square:
-    def __init__(self, size)
+    """
+    A class representing a square.
+
+    Attributes:
+        _size (int or float): The size of the square's sides.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a specified size.
+
+        Args:
+            size (int or float): The size of the square's sides.
+        """
         self._size = size
