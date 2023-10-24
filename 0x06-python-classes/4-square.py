af5751e70f5af4550fcffa85b57dc308bf80e49b
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-24 13:05:48 -0700

    4-main

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""#!/usr/bin/python3 makes the file executable
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square's sides.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square's sides.
+            Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Method named area for calculatiing area of the square
+
+        Return:
+            area of square
+        """
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
