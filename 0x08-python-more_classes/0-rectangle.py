dfa5b088d8770738fc322a619bc734bd15ef5726
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-30 09:16:10 -0700

    0-main

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class definition for Rectangle class"""
+
+
+class Rectangle:
+    """This class defies a rectangle
+    This class doesn't have any specific attributes or methods.
+    """
+    pass
