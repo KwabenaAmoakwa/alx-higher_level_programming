086591daa934de2bc58bf8a7fc21dfc535ab3ff5
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-31 12:05:39 -0700

    19-main

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-"""copy_list module"""
-
-
-def copy_list(l):
-    """
-    makes a copy of a list.
-
-    Args:
-        The list the copy is going to be made of.
-
-    Returns:
-        Copied list
-    """
-    return l[:]
+def copy_list(lst):
+    return lst.copy()
