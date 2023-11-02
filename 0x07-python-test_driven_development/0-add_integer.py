4e22a032eb8536eb50ec16e994326e81d72986cd
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-02 05:15:53 -0700

    0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module for add integer method"""
 
+
 def add_integer(a, b=98):
     """Adds two numbers and returns the sum.
 
@@ -20,6 +21,7 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
