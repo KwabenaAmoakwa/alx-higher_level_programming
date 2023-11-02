ff2e45d6d0810b666efb6af6b5b971de4d98460c
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-02 07:03:23 -0700

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""text_identication module"""
+
+
+def text_indentation(text):
+    """Split the text into paragraphs based on the delimiters '.','?', and ':'.
+
+    The function takes a string `text` as input and splits it into paragraphs
+    based on the presence of the delimiters '.', '?', and ':'. The resulting
+    paragraphs are printed with two newline characters between them.
+
+    Args:
+        text (str): The input text to be split into paragraphs.
+
+    Returns:
+        None
+    """
+    new_str = ""
+    for i in text:
+        if i in ".?:":
+            new_str += i + "|"
+        else:
+            new_str += i
+    ns = new_str.split("| ")
+    print("\n\n".join(ns), end = "")
