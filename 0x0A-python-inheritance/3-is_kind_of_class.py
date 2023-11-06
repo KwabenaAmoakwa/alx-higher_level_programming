641cbaaba2db01c9d2ca6fe2c3aeb6ac8ad0f348
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-06 06:40:22 -0800

    3-main

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance of a particular class or its subclass.
+
+    Args:
+        obj: An object to be checked.
+        a_class: A class to check against.
+
+    Returns:
+        bool: True if the object is an instance of the class or its subclass,
+        False otherwise.
+    """
+    return issubclass(type(obj), a_class)
