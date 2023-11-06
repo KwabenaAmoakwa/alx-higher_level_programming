b054c621fe35e86285f2a9bcd6a2664d8281f4c0
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-06 09:52:31 -0800

    6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Implementation of BaseGeometry module"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        """area method
+
+        Args:
+            None
+
+        Raises:
+            Exception: area() is not implemented
+
+        """
+        raise Exception("area() is not implemented")
