290fb83e27d024be368d692961f8652b11607384
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-06 03:45:13 -0800

    0-main

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""lookup module"""
+def lookup(obj):
+    """lookup returns the list of available attributes and methods of an object.
+
+    Args:
+        obj (any): any class type can be passed
+
+    Returns:
+        list of attributes and methods"""
+    return dir(obj)
