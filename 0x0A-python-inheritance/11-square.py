89f7c11c606d7258a92179a4f8bfecd4d8a51e1b
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-07 02:00:00 -0800

    11-main

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+        self.__width = size
+        self.__height = size
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Sqaure] {}/{}".format(self.__width, self.__height)
