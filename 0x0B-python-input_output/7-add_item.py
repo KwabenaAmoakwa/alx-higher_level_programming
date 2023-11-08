f69345a3724c0ffefb0c0b2bd7a15ee384964edb
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-08 13:06:26 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 
+
 if len(sys.argv) < 2:
+    save_to_json_file([], "add_item.json")
     sys.exit(1)
-newlist += sys.argv[1:]
-save_to_json_file(newlist, "add_item.json")
+data = load_from_json_file("add_item.json")
+data += sys.argv[1:]
+save_to_json_file(data, "add_item.json")
