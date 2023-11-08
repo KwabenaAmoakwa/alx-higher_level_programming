8d964a2564078dde75df3ff6d87b25b4943e7121
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-07 21:13:05 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,16 @@
 
 
 def write_file(filename="", text=""):
+    """writes text to a file.
+
+    Args:
+        filename (file): file to be written to
+        text (string): string to be written
+
+    Returns:
+        int: number of characters written.
+
+    """
     with open(filename, "w") as myfile:
         myfile.write(text)
         return myfile.tell()
