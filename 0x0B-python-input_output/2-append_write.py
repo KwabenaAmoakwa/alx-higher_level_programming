af28198c7b45014e0807d8be17d82fb1f9d45909
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-08 03:47:04 -0800

    2-append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Implementation of the append_write module"""
+
+
+def append_write(filename="", text=""):
+    """appends text to the existing file.
+
+    Args:
+        filename (file): file to be appended
+        text (string): content to append
+
+    Returns:
+        int: the number of characters added
+    """
+    with open(filename, "w+") as myfile:
+        myfile.write(text)
+        return myfile.tell()
