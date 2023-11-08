3f5b5168b1dd6236d531dcfb84d90e553f4be13f
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-08 13:11:48 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""add_item module"""
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
+"""add_item module"""
 
 
 if len(sys.argv) < 2:
