4cf2c6f158b05971c5c9e2e4ab552dc11956503b
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-13 15:44:34 +0000

    Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""Implementation of Base class module"""
+
+
+class Base:
+    """
+    Base class.
+     
+    Attributes:
+            __nb_objects (int): A class variable to track the number of
+            objects created.
+    
+    Methods:
+        __init__(self, id=None): Initializes a Base object with an optional
+        ID.
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """
+        Initializes a Base object.
+        
+        Args:
+            id (int, optional): An optional ID for the object.
+        
+        Note:
+            If no ID is provided, the object's ID will be set using the class
+            variable __nb_objects.
+        
+        Raises:
+            ValueError: If an ID less than 0 is provided.
+        """
+        if id != None:
+            self.__id = id
+        else:
+            __nb_objects += id
+    
