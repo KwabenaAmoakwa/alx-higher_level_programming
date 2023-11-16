562d2b39cea7424871c6cf9a9badd04a5ca02357
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-16 10:33:04 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+"""Implementation of sqaure class module"""
+from models.rectangle import Rectangle
 
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.__size = size
+    
+    def __str__(self):
+        """
+        Display string version of class
+        """
+        s = f"{self.x}/{self.y}"
+        return f"[Rectangle] ({self.id}) {s} - {self.__size}"
