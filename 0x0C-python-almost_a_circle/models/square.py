cbb9b334839b2025ddcf325316bbf8a5a5fa7a09
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-11-16 11:20:51 +0000

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,11 +15,61 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.__size = size
+        self.size = size
 
     def __str__(self):
         """
         Display string version of class
         """
         s = f"{self.x}/{self.y}"
-        return f"[Rectangle] ({self.id}) {s} - {self.__size}"
+        return f"[Square] ({self.id}) {s} - {self.size}"
+    
+    @property
+    def width(self):
+        """
+        int: Width of the square.
+        """
+        return self.size
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for the width property.
+
+        Args:
+            value (int): New width value.
+
+        Raises:
+            ValueError: If value is less than 0.
+            TypeError: If value is not an integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.size = value
+
+    @property
+    def height(self):
+        """
+        int: Height of the rectangle.
+        """
+        return self.size
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for the height property.
+
+        Args:
+            value (int): New height value.
+
+        Raises:
+            ValueError: If value is less than 0.
+            TypeError: If value is not an integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.size = value
