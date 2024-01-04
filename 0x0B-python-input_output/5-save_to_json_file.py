2f5802eb30f1c8df5981abc3f8a9cff4ae8d71f5
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2024-01-04 11:08:28 +0000

    5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,4 +14,5 @@ def save_to_json_file(my_obj, filename):
         None
     """
     with open(filename, "w") as myfile:
-        json.dump(my_obj,myfile)
+        json.dump(my_obj, myfile)
+        
