11deea5755e78748a6b2a4b3a4db5a6b39876c53
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2024-01-10 06:52:07 +0000

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 const arg = process.argv.slice(2);
 
-if (arg[0] === null) {
-  console.log('No argument');
-} else {
+if (arg) {
   console.log(arg[0]);
+} else {
+  console.log('No argument');
 }
