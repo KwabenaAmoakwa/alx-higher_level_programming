b7483f0301b285776d1f45599f5b84dc2bc97f0f
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2024-01-10 16:20:35 +0000

    5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.arg.split(2);
+
+const temp = parseInt(arg[0]);
+
+if (typeof temp !== 'number') {
+    console.log('Not a number');
+} else {
+    console.log('My number: '+temp);
+}
