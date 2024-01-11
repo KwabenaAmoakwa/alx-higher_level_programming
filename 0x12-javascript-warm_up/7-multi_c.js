918ff763f5325775034c28c5124b5d114beec451
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2024-01-11 06:39:55 +0000

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const arg = process.argv.slice(2);
 
 if (isNaN(arg[0]) || arg[0] === undefined) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let index = 0; index < arg[0]; index++) {
-      console.log('C is fun')     
+    console.log('C is fun');  
   }
 }
