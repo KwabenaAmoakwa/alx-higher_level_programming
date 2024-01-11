a1e49ead462e3d0a284d0e2ba51c1950cae83377
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2024-01-11 05:59:49 +0000

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const arg = process.argv.slice(2);
 
 const temp = parseInt(arg[0]);
 
-if (typeof temp === 'number' && arg[0] === undefined) {
+if (!isNaN(integerVal)) {
     console.log('My number: '+temp);
 } else {
     console.log('Not a number');
