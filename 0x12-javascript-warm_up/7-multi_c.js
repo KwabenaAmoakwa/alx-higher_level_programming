13ca7f0c5af55b10e82cd2d2ae37d187b8499912
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2024-01-11 06:40:53 +0000

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,6 +6,6 @@ if (isNaN(arg[0]) || arg[0] === undefined) {
   console.log('Missing number of occurrences');
 } else {
   for (let index = 0; index < arg[0]; index++) {
-    console.log('C is fun');  
+    console.log('C is fun');
   }
 }
